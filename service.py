947517a9174b7b7c5b014556b02a238c42f24501
Carlos Oliveira <carlos.oliveira226@gmail.com>
2018-01-20 02:25:27 -0200

    Adicionado os parametros '-dates', '-parallel' e '-processes_number'.

--- service.py
+++ service.py
@@ -18,6 +18,7 @@ __status__ = "Development"
 import sys
 import os
 import json
+import multiprocessing
 from datetime import datetime
 
 import argparse
@@ -31,12 +32,27 @@ parser = argparse.ArgumentParser(description='''Plataforma de coleta de dados'''
 parser.add_argument("-v", "--verbose", action='store_true', dest='verbose', help="Verbose", default=False)
 parser.add_argument("-configure", type=str, dest='configure', help="Arquivo de configuração",
                     default='{base_dir}/heimdall/configure.json'.format(base_dir=BASE_DIR))
+parser.add_argument("-dates", type=str, nargs=2, dest='dates',
+                    help="Data Inicial e Data Final da busca dos dados historicos\n "
+                         "(apenas para registros de alagamentos). \n"
+                         "Formatos sugerido para a data: YYYYMMDD YYYYMMDD\n"
+                         "(AnoMêsDia)",
+                    default=None)
+parser.add_argument("-parallel", action='store_true', dest='parallel', help="Parallel", default=False)
+
+processes_number = int(multiprocessing.cpu_count() / 2)
+parser.add_argument("-processes_number", type=int, dest='processes_number',
+                    help="Numero de processos para o caso do parametro -parallel ser utilizado.\n"
+                         "Padrão: Metade do numero de processadores da maquina {0}".format(processes_number)
+                    , default=processes_number)
+
 args = parser.parse_args()
 
 if __name__ == "__main__":
     print('[I.{dt:%Y%m%d%H%M}][PID.{pid}] Inicio - {path}'.format(dt=datetime.now(), pid=os.getpid(), path=sys.argv[0]))
 
-    controller.run(configure=json.loads(open(args.configure).read()), verbose=args.verbose)
+    controller.run(boot_settings=json.loads(open(args.configure).read()), processing_dates=args.dates,
+                   parallel=args.parallel, processes_number=args.processes_number, verbose=args.verbose)
 
     print('[I.{dt:%Y%m%d%H%M}][PID.{pid}] Fim - {path}'.format(dt=datetime.now(), pid=os.getpid(), path=sys.argv[0]))
     sys.exit(0)
